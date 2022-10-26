12f3ca5eff9fe716ec6c8a77af6874642f3714bb
JoaoVitor0102 <116230373+JoaoVitor0102@users.noreply.github.com>
2022-10-26 23:05:51 +0000

    aula 2

---
+++ main.py
@@ -0,0 +1,16 @@
+# comando imput() permite que o usuário digite algo 
+
+nome = input ("Digite seu Nome:")
+
+#comando de saída.. exibir na tela
+print (nome)
+#usar o int pra utilizar inteiros 
+idade = int(input ("qual sua idade?"))
+
+print (f"sua idade é {idade} anos")
+
+#mostrar o DOBRO da idade informada
+
+dobro = idade * 2
+
+print (f"o dobro da sua idade é {dobro} anos")
